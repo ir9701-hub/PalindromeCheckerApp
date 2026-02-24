8bbdea530ba7671d915a9b7801390176c01a0c15
ss6909 <ss6909@srmist.edu.in>
2026-02-24 15:54:56 +0530

    Indrajit Added UC2-HardcodePalin

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,5 +2,22 @@ public class PalindromeCheckerApp {
     public static void main (String[] args){
         System.out.println("Welcome to the Palindrome Checker App Management System");
         System.out.println(" Modify this logic to usecase 2");
+        String input = "madam";
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is NOT a Palindrome");
+        }
     }
 }
